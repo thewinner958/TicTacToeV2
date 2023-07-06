6c519b727fe531604c5d8f3063ad79ee8e2d2838
TheWinner <thewinner958@gmail.com>
2023-07-06 19:21:56 +0300

    changed the GameSetup. now you can only give a dimension

--- src/main/java/github/thewinner958/game/GameSetup.java
+++ src/main/java/github/thewinner958/game/GameSetup.java
@@ -7,23 +7,20 @@ public class GameSetup {
     private final String charX;
     private final String charO;
     private final String charEmpty;
-    private final Integer columns;
-    private final Integer rows;
+    private final Integer size;
 
     public GameSetup() {
         charX = "X";
         charO = "O";
         charEmpty = "_";
-        columns = 3;
-        rows = 3;
+        size = 3;
     }
 
-    public GameSetup(String charX, String charO, String charEmpty, Integer columns, Integer rows) {
+    public GameSetup(String charX, String charO, String charEmpty, Integer size) {
         this.charX = charX;
         this.charO = charO;
         this.charEmpty = charEmpty;
-        if (columns < 3 || rows < 3) throw new RuntimeException("You can't have a board less than 3x3");
-        this.columns = columns;
-        this.rows = rows;
+        if (size < 3) throw new RuntimeException("You can't have a board less than 3x3");
+        this.size = size;
     }
 }
