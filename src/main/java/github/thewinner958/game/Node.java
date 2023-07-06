f2f41674ce54a7b8f6b6fdff6d73521019a95fd1
TheWinner <thewinner958@gmail.com>
2023-07-06 19:22:57 +0300

    added a makeMove function

--- src/main/java/github/thewinner958/game/Node.java
+++ src/main/java/github/thewinner958/game/Node.java
@@ -1,13 +1,46 @@
 package github.thewinner958.game;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class Node implements Comparable<Node> {
     private final GameSetup setup;
-    private final boolean isPlayerO; // false - Player X, true - Player O
+    private final boolean isPlayerX; // true - Player X, false - Player O
+    private final Node parent;
     private final String[][] state;
     private final Move move;
     private final List<Node> children;
     private double numVisits, UCTValue, victories, draws, losses = 0;
+    private int winner = GameSimulator.GAME_CONTINUES;
 
+    public Node(GameSetup setup, boolean isPlayerX, Node parent, String[][] state, Move move) {
+        this.setup = setup;
+        this.isPlayerX = isPlayerX;
+        this.parent = parent;
+        this.state = state;
+        this.move = move;
+        children = new ArrayList<>();
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Double.compare(o.UCTValue, UCTValue);
+    }
+
+    void setUCTValue() {
+        if (numVisits == 0) UCTValue = Double.MAX_VALUE;
+        else UCTValue = ((victories + draws / 2) / numVisits) + Math.sqrt(2) * Math.sqrt(Math.log(parent.numVisits) / numVisits);
+    }
+
+    public Node makeMove(Move move) {
+        String[][] state = this.state;
+        if (!state[move.row()][move.column()].equals(setup.getCharEmpty())) throw new RuntimeException("Invalid move");
+        state[move.row()][move.column()] = move.isPlayerX() ? setup.getCharX() : setup.getCharO();
+        return new Node(setup, move.isPlayerX(), this, state, move);
+    }
 }
