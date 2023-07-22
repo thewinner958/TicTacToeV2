7c28be0bad3111a27a686757470a0928447e77a5
TheWinner <thewinner958@gmail.com>
2023-07-22 15:35:47 +0300

    made some things in the game class (wip) and added a todo in the main one to remind myself

--- src/main/java/github/thewinner958/Main.java
+++ src/main/java/github/thewinner958/Main.java
@@ -1,5 +1,6 @@
 package github.thewinner958;
 
+// TODO: 22/07/2023 make it a spring application
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- src/main/java/github/thewinner958/game/Game.java
+++ src/main/java/github/thewinner958/game/Game.java
@@ -1,4 +1,20 @@
 package github.thewinner958.game;
 
+import github.thewinner958.game.player.PlayerInterface;
+import lombok.Getter;
+import lombok.Setter;
+
+// TODO: 22/07/2023 finish this class
+@Getter
+@Setter
 public class Game {
+    private Node state;
+    private final GameSetup setup;
+    private PlayerInterface playerX;
+    private PlayerInterface playerO;
+
+    public Game(GameSetup setup, boolean isPlayerX) {
+        this.setup = setup;
+        state = new Node(this.setup, isPlayerX);
+    }
 }
